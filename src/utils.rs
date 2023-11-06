93eaad63c427deb4e068cf49427bc9c2652fc39f
Nick Furfaro <nfurfaro33@gmail.com>
2023-11-06 13:59:43 -0700

    test: add more tests for util funcs

--- src/utils.rs
+++ src/utils.rs
@@ -90,14 +90,12 @@ pub fn get_bytes_from_token<'a>(token: Token) -> Option<&'a [u8]> {
     }
 }
 
-pub fn replace_bytes(
-    original_bytes: &mut Vec<u8>,
-    start_index: usize,
-    replacement: &[u8],
-) {
-
+pub fn replace_bytes(original_bytes: &mut Vec<u8>, start_index: usize, replacement: &[u8]) {
     let original_operator_length = if original_bytes.len() > start_index + 1 {
-        match &original_bytes[start_index..start_index+2].try_into().unwrap() {
+        match &original_bytes[start_index..start_index + 2]
+            .try_into()
+            .unwrap()
+        {
             b"<=" | b">=" | b"==" | b"!=" | b"<<" | b">>" => 2,
             _ => 1,
         }
@@ -117,258 +115,293 @@ pub fn replace_bytes(
     }
 }
 
-#[test]
-fn test_replace_bytes_equal() {
-    let mut original_bytes = "==".as_bytes().to_vec();
-    let replacement = b"!=";
-    let start_index = 0;
-    replace_bytes(&mut original_bytes, start_index, replacement);
-    assert_eq!(original_bytes, b"!=");
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::File;
+    use std::io::Write;
+    use tempfile::tempdir;
 
-#[test]
-fn test_replace_bytes_not_equal() {
-    let mut original_bytes = "!=".as_bytes().to_vec();
-    let replacement = b"==";
-    let start_index = 0;
-    replace_bytes(&mut original_bytes, start_index, replacement);
-    assert_eq!(original_bytes, b"==");
-}
+    #[test]
+    fn test_find_and_copy_noir_files() {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("test.nr");
+        let mut file = File::create(&file_path).unwrap();
+        writeln!(file, "let x = 42;").unwrap();
+        let result = find_and_copy_noir_files(dir.path()).unwrap();
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0].1.file_name().unwrap(), "_TEMP_test.nr");
+        let copied_file_path = PathBuf::from("./temp/_TEMP_test.nr");
+        assert!(copied_file_path.exists());
+        dir.close().unwrap();
+    }
 
-#[test]
-fn test_replace_bytes_greater_than() {
-    let mut original_bytes = ">".as_bytes().to_vec();
-    let replacement = b"<=";
-    let start_index = 0;
-    replace_bytes(&mut original_bytes, start_index, replacement);
-    assert_eq!(original_bytes, b"<=");
-}
+    #[test]
+    fn test_collect_tokens() {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("test.noir");
+        let mut file = File::create(&file_path).unwrap();
+        writeln!(file, "let x = 42;").unwrap();
+        let temp_noir_files = vec![(file, file_path.clone())];
+        let tokens = collect_tokens(&temp_noir_files).unwrap();
+        assert!(!tokens.is_empty());
+        assert_eq!(tokens[0].1, &file_path);
+        dir.close().unwrap();
+    }
 
-#[test]
-fn test_replace_bytes_greater_than_or_equal_to() {
-    let mut original_bytes = ">=".as_bytes().to_vec();
-    let replacement = b"<";
-    let start_index = 0;
-    replace_bytes(&mut original_bytes, start_index, replacement);
-    assert_eq!(original_bytes, b"<");
-}
+    #[test]
+    fn test_replace_bytes_equal() {
+        let mut original_bytes = "==".as_bytes().to_vec();
+        let replacement = b"!=";
+        let start_index = 0;
+        replace_bytes(&mut original_bytes, start_index, replacement);
+        assert_eq!(original_bytes, b"!=");
+    }
 
-#[test]
-fn test_replace_bytes_less_than() {
-    let mut original_bytes = "<".as_bytes().to_vec();
-    let replacement = b">=";
-    let start_index = 0;
-    replace_bytes(&mut original_bytes, start_index, replacement);
-    assert_eq!(original_bytes, b">=");
-}
+    #[test]
+    fn test_replace_bytes_not_equal() {
+        let mut original_bytes = "!=".as_bytes().to_vec();
+        let replacement = b"==";
+        let start_index = 0;
+        replace_bytes(&mut original_bytes, start_index, replacement);
+        assert_eq!(original_bytes, b"==");
+    }
 
-#[test]
-fn test_replace_bytes_less_than_or_equal_to() {
-    let mut original_bytes = "<=".as_bytes().to_vec();
-    let replacement = b">";
-    let start_index = 0;
-    replace_bytes(&mut original_bytes, start_index, replacement);
-    assert_eq!(original_bytes, b">");
-}
+    #[test]
+    fn test_replace_bytes_greater_than() {
+        let mut original_bytes = ">".as_bytes().to_vec();
+        let replacement = b"<=";
+        let start_index = 0;
+        replace_bytes(&mut original_bytes, start_index, replacement);
+        assert_eq!(original_bytes, b"<=");
+    }
 
-#[test]
-fn test_replace_bytes_and() {
-    let mut original_bytes = "&".as_bytes().to_vec();
-    let replacement = b"|";
-    let start_index = 0;
-    replace_bytes(&mut original_bytes, start_index, replacement);
-    assert_eq!(original_bytes, b"|");
-}
+    #[test]
+    fn test_replace_bytes_greater_than_or_equal_to() {
+        let mut original_bytes = ">=".as_bytes().to_vec();
+        let replacement = b"<";
+        let start_index = 0;
+        replace_bytes(&mut original_bytes, start_index, replacement);
+        assert_eq!(original_bytes, b"<");
+    }
 
-#[test]
-fn test_replace_bytes_or() {
-    let mut original_bytes = "|".as_bytes().to_vec();
-    let replacement = b"&";
-    let start_index = 0;
-    replace_bytes(&mut original_bytes, start_index, replacement);
-    assert_eq!(original_bytes, b"&");
-}
+    #[test]
+    fn test_replace_bytes_less_than() {
+        let mut original_bytes = "<".as_bytes().to_vec();
+        let replacement = b">=";
+        let start_index = 0;
+        replace_bytes(&mut original_bytes, start_index, replacement);
+        assert_eq!(original_bytes, b">=");
+    }
 
-#[test]
-fn test_replace_bytes_xor() {
-    let mut original_bytes = "^".as_bytes().to_vec();
-    let replacement = b"&";
-    let start_index = 0;
-    replace_bytes(&mut original_bytes, start_index, replacement);
-    assert_eq!(original_bytes, b"&");
-}
+    #[test]
+    fn test_replace_bytes_less_than_or_equal_to() {
+        let mut original_bytes = "<=".as_bytes().to_vec();
+        let replacement = b">";
+        let start_index = 0;
+        replace_bytes(&mut original_bytes, start_index, replacement);
+        assert_eq!(original_bytes, b">");
+    }
 
-#[test]
-fn test_replace_bytes_left_shift() {
-    let mut original_bytes = "<<".as_bytes().to_vec();
-    let replacement = b">>";
-    let start_index = 0;
-    replace_bytes(&mut original_bytes, start_index, replacement);
-    assert_eq!(original_bytes, b">>");
-}
+    #[test]
+    fn test_replace_bytes_and() {
+        let mut original_bytes = "&".as_bytes().to_vec();
+        let replacement = b"|";
+        let start_index = 0;
+        replace_bytes(&mut original_bytes, start_index, replacement);
+        assert_eq!(original_bytes, b"|");
+    }
 
-#[test]
-fn test_replace_bytes_right_shift() {
-    let mut original_bytes = ">>".as_bytes().to_vec();
-    let replacement = b"<<";
-    let start_index = 0;
-    replace_bytes(&mut original_bytes, start_index, replacement);
-    assert_eq!(original_bytes, b"<<");
-}
+    #[test]
+    fn test_replace_bytes_or() {
+        let mut original_bytes = "|".as_bytes().to_vec();
+        let replacement = b"&";
+        let start_index = 0;
+        replace_bytes(&mut original_bytes, start_index, replacement);
+        assert_eq!(original_bytes, b"&");
+    }
 
-#[test]
-fn test_replace_bytes_plus() {
-    let mut original_bytes = "+".as_bytes().to_vec();
-    let replacement = b"-";
-    let start_index = 0;
-    replace_bytes(&mut original_bytes, start_index, replacement);
-    assert_eq!(original_bytes, b"-");
-}
+    #[test]
+    fn test_replace_bytes_xor() {
+        let mut original_bytes = "^".as_bytes().to_vec();
+        let replacement = b"&";
+        let start_index = 0;
+        replace_bytes(&mut original_bytes, start_index, replacement);
+        assert_eq!(original_bytes, b"&");
+    }
 
-#[test]
-fn test_replace_bytes_minus() {
-    let mut original_bytes = "-".as_bytes().to_vec();
-    let replacement = b"+";
-    let start_index = 0;
-    replace_bytes(&mut original_bytes, start_index, replacement);
-    assert_eq!(original_bytes, b"+");
-}
+    #[test]
+    fn test_replace_bytes_left_shift() {
+        let mut original_bytes = "<<".as_bytes().to_vec();
+        let replacement = b">>";
+        let start_index = 0;
+        replace_bytes(&mut original_bytes, start_index, replacement);
+        assert_eq!(original_bytes, b">>");
+    }
 
-#[test]
-fn test_replace_bytes_multiply() {
-    let mut original_bytes = "*".as_bytes().to_vec();
-    let replacement = b"/";
-    let start_index = 0;
-    replace_bytes(&mut original_bytes, start_index, replacement);
-    assert_eq!(original_bytes, b"/");
-}
+    #[test]
+    fn test_replace_bytes_right_shift() {
+        let mut original_bytes = ">>".as_bytes().to_vec();
+        let replacement = b"<<";
+        let start_index = 0;
+        replace_bytes(&mut original_bytes, start_index, replacement);
+        assert_eq!(original_bytes, b"<<");
+    }
 
-#[test]
-fn test_replace_bytes_divide() {
-    let mut original_bytes = "/".as_bytes().to_vec();
-    let replacement = b"*";
-    let start_index = 0;
-    replace_bytes(&mut original_bytes, start_index, replacement);
-    assert_eq!(original_bytes, b"*");
-}
+    #[test]
+    fn test_replace_bytes_plus() {
+        let mut original_bytes = "+".as_bytes().to_vec();
+        let replacement = b"-";
+        let start_index = 0;
+        replace_bytes(&mut original_bytes, start_index, replacement);
+        assert_eq!(original_bytes, b"-");
+    }
 
-#[test]
-fn test_replace_bytes_modulo() {
-    let mut original_bytes = "%".as_bytes().to_vec();
-    let replacement = b"*";
-    let start_index = 0;
-    replace_bytes(&mut original_bytes, start_index, replacement);
-    assert_eq!(original_bytes, b"*");
-}
+    #[test]
+    fn test_replace_bytes_minus() {
+        let mut original_bytes = "-".as_bytes().to_vec();
+        let replacement = b"+";
+        let start_index = 0;
+        replace_bytes(&mut original_bytes, start_index, replacement);
+        assert_eq!(original_bytes, b"+");
+    }
 
-#[test]
-fn test_get_bytes_from_token_equal() {
-    let token = Token::Equal;
-    let bytes = get_bytes_from_token(token).unwrap();
-    assert_eq!(bytes, b"==");
-}
+    #[test]
+    fn test_replace_bytes_multiply() {
+        let mut original_bytes = "*".as_bytes().to_vec();
+        let replacement = b"/";
+        let start_index = 0;
+        replace_bytes(&mut original_bytes, start_index, replacement);
+        assert_eq!(original_bytes, b"/");
+    }
 
-#[test]
-fn test_get_bytes_from_token_not_equal() {
-    let token = Token::NotEqual;
-    let bytes = get_bytes_from_token(token).unwrap();
-    assert_eq!(bytes, b"!=");
-}
+    #[test]
+    fn test_replace_bytes_divide() {
+        let mut original_bytes = "/".as_bytes().to_vec();
+        let replacement = b"*";
+        let start_index = 0;
+        replace_bytes(&mut original_bytes, start_index, replacement);
+        assert_eq!(original_bytes, b"*");
+    }
 
-#[test]
-fn test_get_bytes_from_token_less_than() {
-    let token = Token::Less;
-    let bytes = get_bytes_from_token(token).unwrap();
-    assert_eq!(bytes, b"<");
-}
+    #[test]
+    fn test_replace_bytes_modulo() {
+        let mut original_bytes = "%".as_bytes().to_vec();
+        let replacement = b"*";
+        let start_index = 0;
+        replace_bytes(&mut original_bytes, start_index, replacement);
+        assert_eq!(original_bytes, b"*");
+    }
 
-#[test]
-fn test_get_bytes_from_token_less_than_or_equal() {
-    let token = Token::LessEqual;
-    let bytes = get_bytes_from_token(token).unwrap();
-    assert_eq!(bytes, b"<=");
-}
+    #[test]
+    fn test_get_bytes_from_token_equal() {
+        let token = Token::Equal;
+        let bytes = get_bytes_from_token(token).unwrap();
+        assert_eq!(bytes, b"==");
+    }
 
-#[test]
-fn test_get_bytes_from_token_greater_than() {
-    let token = Token::Greater;
-    let bytes = get_bytes_from_token(token).unwrap();
-    assert_eq!(bytes, b">");
-}
+    #[test]
+    fn test_get_bytes_from_token_not_equal() {
+        let token = Token::NotEqual;
+        let bytes = get_bytes_from_token(token).unwrap();
+        assert_eq!(bytes, b"!=");
+    }
 
-#[test]
-fn test_get_bytes_from_token_greater_than_or_equal() {
-    let token = Token::GreaterEqual;
-    let bytes = get_bytes_from_token(token).unwrap();
-    assert_eq!(bytes, b">=");
-}
+    #[test]
+    fn test_get_bytes_from_token_less_than() {
+        let token = Token::Less;
+        let bytes = get_bytes_from_token(token).unwrap();
+        assert_eq!(bytes, b"<");
+    }
 
-#[test]
-fn test_get_bytes_from_token_and() {
-    let token = Token::Ampersand;
-    let bytes = get_bytes_from_token(token).unwrap();
-    assert_eq!(bytes, b"&");
-}
+    #[test]
+    fn test_get_bytes_from_token_less_than_or_equal() {
+        let token = Token::LessEqual;
+        let bytes = get_bytes_from_token(token).unwrap();
+        assert_eq!(bytes, b"<=");
+    }
 
-#[test]
-fn test_get_bytes_from_token_or() {
-    let token = Token::Pipe;
-    let bytes = get_bytes_from_token(token).unwrap();
-    assert_eq!(bytes, b"|");
-}
+    #[test]
+    fn test_get_bytes_from_token_greater_than() {
+        let token = Token::Greater;
+        let bytes = get_bytes_from_token(token).unwrap();
+        assert_eq!(bytes, b">");
+    }
 
-#[test]
-fn test_get_bytes_from_token_xor() {
-    let token = Token::Caret;
-    let bytes = get_bytes_from_token(token).unwrap();
-    assert_eq!(bytes, b"^");
-}
+    #[test]
+    fn test_get_bytes_from_token_greater_than_or_equal() {
+        let token = Token::GreaterEqual;
+        let bytes = get_bytes_from_token(token).unwrap();
+        assert_eq!(bytes, b">=");
+    }
 
-#[test]
-fn test_get_bytes_from_token_left_shift() {
-    let token = Token::ShiftLeft;
-    let bytes = get_bytes_from_token(token).unwrap();
-    assert_eq!(bytes, b"<<");
-}
+    #[test]
+    fn test_get_bytes_from_token_and() {
+        let token = Token::Ampersand;
+        let bytes = get_bytes_from_token(token).unwrap();
+        assert_eq!(bytes, b"&");
+    }
 
-#[test]
-fn test_get_bytes_from_token_right_shift() {
-    let token = Token::ShiftRight;
-    let bytes = get_bytes_from_token(token).unwrap();
-    assert_eq!(bytes, b">>");
-}
+    #[test]
+    fn test_get_bytes_from_token_or() {
+        let token = Token::Pipe;
+        let bytes = get_bytes_from_token(token).unwrap();
+        assert_eq!(bytes, b"|");
+    }
 
-#[test]
-fn test_get_bytes_from_token_plus() {
-    let token = Token::Plus;
-    let bytes = get_bytes_from_token(token).unwrap();
-    assert_eq!(bytes, b"+");
-}
+    #[test]
+    fn test_get_bytes_from_token_xor() {
+        let token = Token::Caret;
+        let bytes = get_bytes_from_token(token).unwrap();
+        assert_eq!(bytes, b"^");
+    }
 
-#[test]
-fn test_get_bytes_from_token_minus() {
-    let token = Token::Minus;
-    let bytes = get_bytes_from_token(token).unwrap();
-    assert_eq!(bytes, b"-");
-}
+    #[test]
+    fn test_get_bytes_from_token_left_shift() {
+        let token = Token::ShiftLeft;
+        let bytes = get_bytes_from_token(token).unwrap();
+        assert_eq!(bytes, b"<<");
+    }
 
-#[test]
-fn test_get_bytes_from_token_multiply() {
-    let token = Token::Star;
-    let bytes = get_bytes_from_token(token).unwrap();
-    assert_eq!(bytes, b"*");
-}
+    #[test]
+    fn test_get_bytes_from_token_right_shift() {
+        let token = Token::ShiftRight;
+        let bytes = get_bytes_from_token(token).unwrap();
+        assert_eq!(bytes, b">>");
+    }
 
-#[test]
-fn test_get_bytes_from_token_divide() {
-    let token = Token::Slash;
-    let bytes = get_bytes_from_token(token).unwrap();
-    assert_eq!(bytes, b"/");
-}
+    #[test]
+    fn test_get_bytes_from_token_plus() {
+        let token = Token::Plus;
+        let bytes = get_bytes_from_token(token).unwrap();
+        assert_eq!(bytes, b"+");
+    }
 
-#[test]
-fn test_get_bytes_from_token_modulo() {
-    let token = Token::Percent;
-    let bytes = get_bytes_from_token(token).unwrap();
-    assert_eq!(bytes, b"%");
+    #[test]
+    fn test_get_bytes_from_token_minus() {
+        let token = Token::Minus;
+        let bytes = get_bytes_from_token(token).unwrap();
+        assert_eq!(bytes, b"-");
+    }
+
+    #[test]
+    fn test_get_bytes_from_token_multiply() {
+        let token = Token::Star;
+        let bytes = get_bytes_from_token(token).unwrap();
+        assert_eq!(bytes, b"*");
+    }
+
+    #[test]
+    fn test_get_bytes_from_token_divide() {
+        let token = Token::Slash;
+        let bytes = get_bytes_from_token(token).unwrap();
+        assert_eq!(bytes, b"/");
+    }
+
+    #[test]
+    fn test_get_bytes_from_token_modulo() {
+        let token = Token::Percent;
+        let bytes = get_bytes_from_token(token).unwrap();
+        assert_eq!(bytes, b"%");
+    }
 }
