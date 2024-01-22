53dccea2930c8c5f78d13bba50224ce191000d73
Nick Furfaro <nfurfaro33@gmail.com>
2024-01-22 14:54:33 -0700

    feat: update install.sh to give choice of binaries

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
-s
+
 # Define the version
-version="1.0.0"
+version="v1.0.0"
+
+# List the available binaries
+echo "Please select your binary:"
+echo "1) hunter_x86_64-unknown-linux-gnu"
+echo "2) hunter_aarch64_apple_darwin"
+read -p "Selection: " selection
+
+# Set the binary name based on the selection
+case $selection in
+    1) binary="hunter_x86_64-unknown-linux-gnu";;
+    2) binary="hunter_aarch64_apple_darwin";;
+    *) echo "Invalid selection"; exit 1;;
+esac
 
 # Download the binary using cURL
-curl -LO https://github.com/nfurfaro/hunter/releases/download/$version/hunter
+curl -LO https://github.com/nfurfaro/hunter/releases/download/$version/$binary
 
 # Make the binary executable
-chmod +x hunter
+chmod +x $binary
 
 # Move the binary to a directory in your PATH
-mv hunter /usr/local/bin
+mv $binary /usr/local/bin/hunter
 
 # Confirm installation
 if command -v hunter &> /dev/null
